f704d092a8aee52a8a9c4baedc5bb1a507f02e59
Alejandro Navarro <alenvbn@gmail.com>
2023-07-12 16:09:58 -0600

    modify button favorites for mobile screens

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -56,18 +56,25 @@ const PokemonByNamePage: NextPage<Props> = ({ pokemon }) => {
         <Grid xs={12} sm={8}>
           <Card>
             <Card.Header
-              css={{ display: 'flex', justifyContent: 'space-between' }}
+            // css={{ display: 'flex', justifyContent: 'space-between' }}
             >
-              <Text h1 transform="capitalize">
-                {pokemon.name}
-              </Text>
-              <Button
-                color="gradient"
-                ghost={!isInFavorites}
-                onPress={onToggleFavorite}
+              <Container
+                justify="space-between"
+                direction="row"
+                display="flex"
+                alignItems="center"
               >
-                {isInFavorites ? 'En Favoritos' : 'Agregar a Favoritos'}
-              </Button>
+                <Text h1 transform="capitalize">
+                  {pokemon.name}
+                </Text>
+                <Button
+                  color="gradient"
+                  ghost={!isInFavorites}
+                  onPress={onToggleFavorite}
+                >
+                  {isInFavorites ? 'En Favoritos' : 'Agregar a Favoritos'}
+                </Button>
+              </Container>
             </Card.Header>
             <Card.Body>
               <Text size={30}>Sprites:</Text>
